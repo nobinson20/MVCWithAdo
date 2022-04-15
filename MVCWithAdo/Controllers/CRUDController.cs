34d17aade523af0da9d796961505bbc5ca2c6ee7
nobinson20 <nobinson20@gmail.com>
2022-04-15 19:36:36 +0900

    modified default controller name to 'CRUD'

---
+++ MVCWithAdo/Controllers/CRUDController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithAdo.Controllers
+{
+    public class CRUDController : Controller
+    {
+        // GET: CRUD
+        public ActionResult Index()
+        {
+            return View("Home");
+        }
+    }
+}
